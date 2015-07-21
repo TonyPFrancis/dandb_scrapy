ca83e97495e0f40cc2f78db674c42401fa575706
TonyPFrancis <tony.612.4u@gmail.com>
2015-07-21 22:26:12 +0530

    adding spider

---
+++ dandb/dandb/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for dandb project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'dandb'
+
+SPIDER_MODULES = ['dandb.spiders']
+NEWSPIDER_MODULE = 'dandb.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'dandb (+http://www.yourdomain.com)'
