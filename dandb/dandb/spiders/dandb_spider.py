90cea6c499eff95afad294721f37c246d07f465c
TonyPFrancis <tony.612.4u@gmail.com>
2015-07-21 23:13:55 +0530

    adding spider file

---
+++ dandb/dandb/spiders/dandb_spider.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from apt.utils import get_maintenance_end_date
+import re
+import requests
+from scrapy.spider import Spider
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from urlparse import urlparse, urljoin, parse_qs
+from scrapy.selector import Selector
+from time import sleep
+import urllib
+from dandb.items import DandbItem
+from scrapy.http import Request
+from dateutil import rrule, parser
+from dateutil import relativedelta
+from datetime import timedelta, datetime
+from scrapy.log import ScrapyFileLogObserver
+from scrapy import log
+from scrapy.shell import inspect_response
+import time
+import json
+
